210983db5fbb058f1e5a398b4e987779a9964fd7
Laki Peter <lakipeter85@gmail.com>
2021-02-20 16:53:37 +0100

    Adding Feature: Explosion effect when destroying enemy

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,6 +9,7 @@ public class Player : MonoBehaviour
     public GameObject line;
     private float jumpForce = 100;
     public Rigidbody2D player;
+    public ParticleSystem particleEffect;
 
     // Visuals
     public LineRenderer lr;
@@ -26,29 +27,22 @@ public class Player : MonoBehaviour
         player.freezeRotation = true;
         Vector3 mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         if (Input.GetMouseButton(0)) 
-        {
-            
+        {      
             DrawLine(mousepos);
             Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y);
             Vector3 lookPos = Camera.main.ScreenToWorldPoint(mousePos);
             lookPos = lookPos - transform.position;
             float angle = Mathf.Atan2(lookPos.y, lookPos.x) * Mathf.Rad2Deg;
             player.transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
-            
         }
-        
         if (Input.GetMouseButtonUp(0))
         {
             lr.enabled = false;
             if (!readyToJump) return;
             float force = Vector3.Distance(player.transform.position, mousepos);
             player.velocity = player.transform.up * force * 1.5f;
-            readyToJump = false;
-            
-            
-        }
-
-       
+            readyToJump = false;            
+        }       
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -58,12 +52,12 @@ public class Player : MonoBehaviour
             Vector3 direction = new Vector3(collision.GetContact(0).point.x, collision.GetContact(0).point.y - 0.4f) - player.transform.position;
             direction = -direction;
             player.velocity = direction * 30;
-            Destroy(collision.gameObject);
+            particleEffect.Stop();
+            particleEffect.transform.position = collision.gameObject.transform.position;
+            particleEffect.Play();
+            Destroy(collision.gameObject);           
         }
-
-
     }
-
     void DrawLine(Vector3 mousepos)
     {
         lr = line.GetComponent<LineRenderer>();
@@ -75,6 +69,4 @@ public class Player : MonoBehaviour
         lr.SetPosition(0, player.transform.position);
         lr.SetPosition(1, mousepos);
     }
-    
-
 }
